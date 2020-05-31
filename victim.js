24e675142cdabb4e5787a920a56e9b7c5dd1a826
truptimehta <63162871+truptimehta@users.noreply.github.com>
2020-05-31 11:32:01 +0530

    Update victim.js

--- victim.js
+++ victim.js
@@ -7,6 +7,7 @@ this.state=state;
 this.severity=severity
 }
 getHospitalInfo(){
+    console.log("Getting info" + this.highwayno);
 var ref=database.ref('highways')
 ref.orderByKey().equalTo(this.highwayno).on("value",(data)=>{
     if(allHospitals!==undefined){
